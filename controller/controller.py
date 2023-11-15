5c58cfacd8c94b851715de64c0f9fcba93544660
CAR-hash <lejian663@126.com>
2023-11-15 19:16:24 +0800

    ignore minial

--- controller/controller.py
+++ controller/controller.py
@@ -1,5 +1,5 @@
 from drive.disk import SimpleDisk
-
+import os
 class BaseController(object):
     def __init__(self, config):
         self.config = config
@@ -19,6 +19,31 @@ class SimpleController(BaseController):
         self.data_objs = {}
 
     def create_new_raid(self):
-        print(self.config)
+        assert self.config.disk_size % self.config.r == 0
+
+        if not os.path.exists(self.config.name):
+            os.mkdir(self.config.name)
+
+        disk_meta = {
+            "raid_path" : self.config.name,
+            "file_size" : self.config.file_size,
+            "chunk_size" : self.config.r,
+            "disk_size" : self.config.disk_size,
+            "chunk_per_disk" : self.config.disk_size / self.config.r,
+        }
         for i in range(0, self.stripe_count + self.parity_count):
-            disk = SimpleDisk(disk_id=i)
+            disk = SimpleDisk(disk_id=i, disk_meta=disk_meta)
+            disk.allocate()
+
+    def activate_raid(self):
+        assert self.config.disk_size % self.config.r == 0
+        disk_meta = {
+            "raid_path" : self.config.raid_path,
+            "file_size" : self.config.file_size,
+            "chunk_size" : self.config.r,
+            "disk_size" : self.config.disk_size,
+            "chunk_per_disk" : self.config.disk_size / self.config.r,
+        }
+        for i in range(0, self.stripe_count + self.parity_count):
+            disk = SimpleDisk(disk_id=i, disk_meta=disk_meta)
+            disk.allocate()
--- drive/disk.py
+++ drive/disk.py
@@ -1,27 +1,67 @@
+import copy
 import os
 import json
-RAID_PATH = "disk"
+
 
 # local disk
 # A disk is consist fo a certain number of chunks
 # A disk is consist of  a json file meta.json and several content files.
 class SimpleDisk(object):
-    def __init__(self, disk_id: int):
-        self.disk_id = disk_id
+    def __init__(self, disk_id: int, disk_meta):
         self.path = ""
-        self.allocate()
+        self.meta = copy.deepcopy(disk_meta)
+
+        self.chunk_per_disk = int(self.meta["disk_size"]/self.meta["chunk_size"])
+        self.file_count = int(self.meta["disk_size"]/self.meta["file_size"])
+        self.chunk_per_file = int(self.meta["file_size"]/self.meta["chunk_size"])
+        self.chunk_size = self.meta["chunk_size"]
+
+        self.file_path = ""
+        self.file_size = self.meta["file_size"]
+
+        self.disk_id = disk_id
 
     def allocate(self):
-        disk_path = RAID_PATH + "\\%d" % self.disk_id
-        print(disk_path)
+        disk_path = self.meta["raid_path"] + "\\%d" % self.disk_id
         if os.path.exists(disk_path):
             raise Exception("Disk initialization failed since location %s has been occupied." % disk_path)
         else:
             os.mkdir(disk_path)
-            self.path = disk_path
-            file_path = disk_path + "\\content"
-            # create meta
 
+            #update meta
+            self.meta["id"] = self.disk_id
+
+            self.path = disk_path
+            self.meta["path"] = disk_path
+
+            for i in range(0, self.chunk_per_disk):
+                self.meta["chunk_%d" % i] = False
+
+            file_path = disk_path + "\\content"
+            self.file_path = file_path
+
+            # create meta
+            meta_path = disk_path + "\\meta.json"
+            with open(meta_path, "w+") as f:
+                f.write(json.dumps(self.meta))
             # create disk
-            open(file_path, "w+")
+            for i in range(0, self.file_count):
+                with open(file_path + "_%d" % i, "w+") as f:
+                    init_data = bytes(self.file_size)
+                    f.write(str(init_data, encoding="utf-8"))
+
             print("Disk %d is initialized at %s." % (self.disk_id, self.path))
+
+    # read meta
+    def activate(self):
+        pass
+
+    def write_chunk(self, chunk_idx, chunk):
+        # locate chunk
+        outer_idx = chunk_idx / self.chunk_per_file
+        inner_idx = chunk_idx % self.chunk_per_file
+        # write
+        with open(self.file_path + "_%d" % outer_idx) as f:
+            data = f.read()
+            data[inner_idx*self.chunk_size:(inner_idx+1)*self.chunk_size] = chunk
+        pass
